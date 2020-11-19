fed74f749a279df9427019a7cb51bf9138ea2a5c
hyunsukimsokcho <hskimsokcho@gmail.com>
2020-11-19 16:02:38 +0900

    [EM] reuse em-test

--- bingo/test-em.py
+++ bingo/test-em.py
@@ -11,6 +11,8 @@ BINGO_DIR = os.path.join(PROJECT_HOME, 'bingo')
 RUN_BIN = os.path.join(PROJECT_HOME, 'bin', 'run.py')
 RULE_PROB_TOK = 'rule-prob-'
 FG_TOK = 'factor-graph-'
+BINGO_STATS_TOK = 'bingo_stats-'
+SPARROW_OUT = 'sparrow-out'
 
 
 interval_benchmarks = [
@@ -58,21 +60,45 @@ def find_last_rule_prob(bench_name):
     return os.path.join(src_dir, RULE_PROB_TOK + str(last) + '.txt')
 
 
+def get_bench_dir(bench_name_ver):
+    return os.path.join(BENCHMARK_DIR, bench_name_ver)
+
+def get_bingo_stats_path(bench_dir, timestamp):
+    return os.path.join(bench_dir, SPARROW_OUT, analysis_type, BINGO_STATS_TOK + timestamp + '.txt')
+
 def run_em_train():
+    print("Computing TRAIN results begins..")
+    train_score = 0
     for tb in training_benchs:
         tb_name = tb.split('/')[0]
         ts = make_timestamp(tb_name, 'train')
-        last_fg_file = find_last_fg(tb_name)
-
+        last_rule_prob_file = find_last_rule_prob(target_program)
+        train_bench_dir = get_bench_dir(tb)
+        em_train_cmd = [ RUN_BIN, "em-test", train_bench_dir, last_rule_prob_file, '--timestamp', ts ]
+        p = subprocess.Popen(em_train_cmd)
+        p.communicate()
+        bingo_stats_path = get_bingo_stats_path(train_bench_dir, ts)
+        with open(bingo_stats_path, 'r') as f:
+            score = len(f.readlines()) - 1
+            print(tb_name, score)
+            train_score += score
+    print("- Train score:", train_score)
 
 def run_em_test():
+    print("Computing TEST results begins..")
     ts = make_timestamp(None, 'test')
     last_rule_prob_file = find_last_rule_prob(target_program)
-    em_test_cmd = [ RUN_BIN, "em-test", os.path.join(BENCHMARK_DIR, test_bench), last_rule_prob_file, '--timestamp', ts ]
+    test_bench_dir = get_bench_dir(test_bench)
+    em_test_cmd = [ RUN_BIN, "em-test", test_bench_dir, last_rule_prob_file, '--timestamp', ts ]
     p = subprocess.Popen(em_test_cmd)
     p.communicate()
+    bingo_stats_path = get_bingo_stats_path(test_bench_dir, ts)
+    with open(bingo_stats_path, 'r') as f:
+        test_score = len(f.readlines()) - 1
+        print("- Test score:", test_score)
 
 
 if __name__ == "__main__":
+    print("EM results for", target_program)
     run_em_test()
-    #run_em_train()
+    run_em_train()
